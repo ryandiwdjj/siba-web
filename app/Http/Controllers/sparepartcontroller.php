358f002ef3f1cdc63b5473cefba686c54b9d812b
Ryandi Widjaja <kryandiw@gmail.com>
2019-04-07 18:43:52 +0700

    upload picture done (untested)

--- app/Http/Controllers/sparepartcontroller.php
+++ app/Http/Controllers/sparepartcontroller.php
@@ -37,24 +37,53 @@ class sparepartcontroller extends Controller
      */
     public function store(Request $request)
     {
+        $v = Validator::make($request->all(), [
+            'kode_sparepart' => 'required',
+            'nama_sparepart' => 'required',
+            'merk_sparepart' => 'required',
+            'tipe_sparepart' => 'required',
+            'gambar_sparepart' => 'required|image',
+            'jumlah_stok_sparepart' => 'required|numeric',
+            'harga_beli_sparepart' => 'required|numeric',
+            'harga_jual_sparepart' => 'required|numeric',
+            'jumlah_minimal' => 'required|numeric',
+        ]);
+
+        if ($v->fails())
+        {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $v->errors()
+            ], 422);
+        }
+
+
+
         $sparepart = new sparepart;
+
         $sparepart->kode_sparepart = $request->kode_sparepart;
         $sparepart->nama_sparepart = $request->nama_sparepart;
-        $sparepart->merk_sparepart = $request->merk_sparepart;
+        $sparepart->merk_sparepart = $request->merk_sparepart;  
         $sparepart->tipe_sparepart = $request->tipe_sparepart;
-        $sparepart->gambar_sparepart = $request->gambar_sparepart;
+        //gambar
         $sparepart->jumlah_stok_sparepart = $request->jumlah_stok_sparepart;
         $sparepart->harga_beli_sparepart = $request->harga_beli_sparepart;
         $sparepart->harga_jual_sparepart = $request->harga_jual_sparepart;
         $sparepart->jumlah_minimal = $request->jumlah_minimal;
 
-        $success = $sparepart->save();
-
-        if (!$success) {
-            return response()->json('Error Saving', 500);
-        } else {
-            return response()->json('Success', 204);
+        if($request->hasFile('gambar_sparepart')){
+            $dir = 'uploads/';
+            $path = 'https://localhost:8000/uploads/';
+            $extension = strtolower($request->file('gambar_sparepart')->getClientOriginalExtension());
+            $fileName = str_random() . '.' . $extension;
+            $file = $path . $fileName;
+            $request->file('gambar_sparepart')->move($dir, $fileName);
+            $sparepart->gambar_sparepart = $file;
         }
+
+        $sparepart->save();
+
+        return response()->json(['status' => 'success creating sparepart'], 201);
     }
 
     /**
