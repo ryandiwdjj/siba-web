19283a4206c96b2634cdffe1308c71ce835f9a1c
Ryandi Widjaja <kryandiw@gmail.com>
2019-05-12 18:27:59 +0700

    grafik pendapatan bulanan
    data masih belum bisa

--- resources/js/components/admin/laporan/PendapatanBulanan.vue
+++ resources/js/components/admin/laporan/PendapatanBulanan.vue
@@ -9,6 +9,7 @@
 
                   <div class="card-tools">
                       <router-link to="/laporan/cetak/pendapatan_bulanan" class="button is-success">Cetak</router-link>
+                      <h6 class="card-tools" id="tanggal">Tanggal dan waktu generate</h6>
                   </div>
                     
                     <div class="card-body table-responsive p-0">
@@ -33,14 +34,14 @@
                       </tr>
                     </tbody>
                     </table>     
-                    <vue-simple-spinner v-if="loading"></vue-simple-spinner>            
+                    <vue-simple-spinner v-if="loading"></vue-simple-spinner>     
+                    <canvas id="myChart" width="1092" height="400"></canvas>       
                   </div>     
             </div>
         </div>
     </div>
 </div>
 </template>
-
 
 <script>
   export default {
@@ -51,9 +52,68 @@
         loading: true
       }
     },
+    function :{
+      timeNow(i) {
+        var d = new Date(),
+          h = (d.getHours()<10?'0':'') + d.getHours(),
+          m = (d.getMinutes()<10?'0':'') + d.getMinutes();
+        i.value = h + ':' + m;
+      }
+    },
+    created() {
+      var app = this;
+      
+    },
     mounted()  {
      var app = this;
     app.getResults();
+
+    var jan = app.getGrandTotalChartData(0);
+
+    console.log(jan);
+
+    this.$nextTick(function() {
+      window.chart = require('chart.js');
+
+      var ctx = document.getElementById("myChart").getContext("2d");
+
+var data = {
+  labels: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"],
+  datasets: [{
+    label: "Service",
+    backgroundColor: "blue",
+    data: [3, 4, 5, 3, 9, 7, 8, 4, 8, 3, 5, 1] //data nya urutan dari jan sampe des
+  }, {
+    label: "Spareparts",
+    backgroundColor: "red",
+    data: [4, 3, 5, 5, 7, 3, 8, 5, 2, 6, 4, 9]
+  }, {
+    label: "Total",
+    backgroundColor: "grey",
+    data: [7, 2, 6, 6, 2, 4, 8, 6, 4, 6, 9, 3]
+  }]
+};
+
+var myBarChart = new Chart(ctx, {
+  type: 'bar',
+  data: data,
+  options: {
+  	legend: {
+    	position: 'right'
+    },
+    barValueSpacing: 20,
+    scales: {
+      yAxes: [{
+        ticks: {
+          min: 0,
+        }
+      }]
+    }
+  }
+});
+
+    })
+
     },
     computed: {
        filteredList: function(){
@@ -67,6 +127,21 @@
         .then(function(resp){
         app.transaksiPenjualan = resp.data;
         app.loading = false;
+        // console.log(app.transaksiPenjualan);
+        })
+        .catch(function(resp){
+          console.log(resp);
+          app.loading = false;
+         
+        })
+      },
+      getGrandTotalChartData(month){
+        var app = this;
+        axios.get('/api/report/pendapatan_bulanan')
+        .then(function(resp){
+        app.grand_total_trans = resp.data[month].grand_total;
+        app.loading = false;
+        console.log(app.grand_total_trans);
         })
         .catch(function(resp){
           console.log(resp);
@@ -83,5 +158,6 @@
       }
     }
   }
+  
 
 </script>
